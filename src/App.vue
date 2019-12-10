e637d4f0b75cb10b2a8bd1ec3af7dd3f70b56a63
João Pedro Cavalcanti <jpbdvmc@gmail.com>
2019-12-10 15:49:50 -0300

    commit1

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>Conversor de Moedas</h1>
+    <div class="linha">
+      <Conversor moedaA="USD" moedaB="BRL"/>
+      <Conversor moedaA="BRL" moedaB="USD"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Conversor from "componenteconversor/Conversor.vue"
+
+export default {
+  name: 'app',
+  components: {
+    Conversor
+  }
+}
+</script>
+
+<style>
+#app {
+  max-width: 900px;
+  margin: auto;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.linha{
+  display: flex;
+  justify-content: space-around;
+  margin-top: 40px;
+}
+</style>
